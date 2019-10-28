a2a9ce38b90dabc6f4ba92b31d638caa5e109421
Dinhtu99 <devdinhtu@gmail.com>
2019-10-28 22:56:43 +0700

    first commit

---
+++ product_detail.php
@@ -0,0 +1,13 @@
+<?php
+	include('connect/connect.php');
+	$id = $_GET['id'];
+
+	$products = $mysqli->query("SELECT p.id,p.name as nameProduct ,p.id_type as idType, t.name as nameType, p.price, p.color, p.material, p.description, GROUP_CONCAT(i.link) AS image FROM product p LEFT JOIN images i ON p.id = i.id_product inner join product_type t ON t.id = p.id_type where p.new = 1 and p.id=$id");
+	while ($row = $products->fetch_object()){
+	    $product[] = $row;
+	}
+
+	echo json_encode($product);
+
+
+?>
